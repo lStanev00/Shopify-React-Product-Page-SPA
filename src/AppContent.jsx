675ae4e7734053de74ba0d6fed7525705ce214dd
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 20:30:54 +0300

    global css for the top widget element

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { ContextVariables } from "./assets/context-variables/ContextVariables";
 import ProductMain from "./assets/Sections/Product/ProductMain";
+import "./assets/Styles/Main.css"
 
 export default  function AppContent() {
   const { product, setProduct, fetchProduct } = useContext(ContextVariables);
