9fbcb1658afd1756657f96bf900e723303976439
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 18:27:14 +0300

    ContextVariables for product data travel across the widget

---
+++ src/assets/context-variables/ContextVariables.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const ContextVariables = createContext();
+
+export const ContextProvider = ({children}) => {
+    const url = "https://shopify-rest-api-demo-production.up.railway.app";
+    const [product, setProduct] = useState(undefined);
+
+    async function fetchProduct(productSlug) {
+        const endpoint = url + `/product/belgian-hazelnut-cookie`
+
+        const res = await fetch(endpoint);
+
+        if (!res.ok) return null;
+        if(res.status == 200) {
+            const data = await res.json();
+            return data
+        }
+        return undefined
+    }
+
+    return (
+        <ContextVariables.Provider value={{product, setProduct, fetchProduct}}>
+            {children}
+        </ContextVariables.Provider>
+    )
+
+}
