54fe7963ad12973cc7c2e3527a81dc0f45717eb5
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 22:32:11 +0300

    new review stars + info + css

--- src/assets/Sections/Product/ProductMain.jsx
+++ src/assets/Sections/Product/ProductMain.jsx
@@ -1,11 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { ContextVariables } from "../../context-variables/ContextVariables";
 import Style from "../../Styles/ProductMain.module.css";
+import {svg} from "../../Components/svgs"
 
 export default function ProductMain() {
   const { product } = useContext(ContextVariables);
   const [mainImage, setMainImage] = useState(product?.media[0]);
   const [imageList, setImageList] = useState(undefined);
+  const [rating, setRating] = useState(4.9);
+  const [starCount, setStarCount] = useState(Math.round(rating));
+  const [reviewsCount, Reviews] = useState(102);
 
   useEffect(() => {if (product) setImageList(product?.media)}, [product]);
 
@@ -46,6 +50,23 @@ export default function ProductMain() {
             <div className={Style.contentHeader}>
                 <span className={Style.productName}>{product.title}</span>
                 <span className={Style.productPrice}>{product.price.amount} {product.price.currencyCode}</span>
+            </div>
+
+            <div className={Style.productDescriptionContent}>
+                <div className={Style.contentReviews}>
+                    <div className={Style.startsDiv}>
+                        {starCount && (
+                            [...Array(starCount)].map((dummy, i) => {
+                                return (
+                                    <>
+                                    <div className={Style.starDiv} key={`star${i}`}><svg.FullStar /></div>
+                                    </>
+                                )
+                            })
+                        )}
+                    </div>
+                    <div className={Style.reviewCounts}>{rating} ( {reviewsCount} reviews )</div>
+                </div>
             </div>
 
           </div>
