729a6ec073512f3f26f068514dd067b594c37c5a
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 00:22:05 +0300

    slug of the item extracted

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,14 +1,15 @@
 import { useContext, useEffect } from "react";
 import { ContextVariables } from "./assets/context-variables/ContextVariables";
 import ProductMain from "./assets/Sections/Product/ProductMain";
-import "./assets/Styles/Main.css"
+import "./assets/Styles/Main.css";
 
-export default  function AppContent() {
+export default function AppContent() {
   const { product, setProduct, fetchProduct } = useContext(ContextVariables);
 
   useEffect(() => {
     async function loadProduct() {
-      const data = await fetchProduct("implement");
+      const slug = window.location.pathname.replace("/products/", ``);
+      const data = await fetchProduct("");
       setProduct(data);
     }
     loadProduct();
