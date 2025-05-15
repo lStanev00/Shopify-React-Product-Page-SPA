8a341a9faf3728cbbe33ffa15b7dcb82d630222b
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-16 02:35:07 +0300

    bugfix splice on a usestate

--- src/assets/helpers/paginateReviews.js
+++ src/assets/helpers/paginateReviews.js
@@ -1,12 +1,15 @@
 export function paginateReviews(reviewsArray) {
-    if (!reviewsArray ||reviewsArray.length < 11) return [reviewsArray]
-    const result = [];
-    const iterations = Math.floor(reviewsArray.length / 10);
+    if (!Array.isArray(reviewsArray) || reviewsArray.length < 11) return [reviewsArray];
 
+    const result = [];
     const desiredPageLength = 10;
+    const total = reviewsArray.length;
+    const iterations = Math.ceil(total / desiredPageLength);
 
     for (let i = 0; i < iterations; i++) {
-        let page = reviewsArray.splice(0, desiredPageLength);
+        let start = i * desiredPageLength;
+        let end = start + desiredPageLength;
+        let page = reviewsArray.slice(start, end);
         result.push(page);
     }
 
