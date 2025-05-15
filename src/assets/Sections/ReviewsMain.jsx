eace61fbcbfaf189d8807192df1ad9b7b93de60b
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 11:54:43 +0300

    enw reviews section and css

--- src/assets/Sections/ReviewsMain.jsx
+++ src/assets/Sections/ReviewsMain.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ContextVariables } from "../context-variables/ContextVariables";
+import Style from "../Styles/ReviewsMain.module.css";
 
 export default function ReviewsMain () {
     const { product, paginatedData, setPaginatedData } = useContext(ContextVariables);
@@ -7,7 +8,17 @@ export default function ReviewsMain () {
     if (paginatedData) {
         return(
             <>
-                <section className={Style.mainSection}></section>
+                <section className={Style.mainSection}>
+                    <p className={Style.headerP}>Customer reviews</p>
+
+                    <div className={Style.reviewsContent}>
+                        <div className={Style.contentHeader}>
+                            <div className={Style.currentStarCount}>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </>
         )
     }
