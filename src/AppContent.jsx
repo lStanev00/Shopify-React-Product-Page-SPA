752aeeb425a978c01494dc77882aa77b6a359d5e
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 11:06:35 +0300

    paginating the data for the reviews container

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -3,15 +3,17 @@ import { ContextVariables } from "./assets/context-variables/ContextVariables";
 import ProductMain from "./assets/Sections/Product/ProductMain";
 import "./assets/Styles/Main.css";
 import { getVisitorId } from "./assets/helpers/getVisitorId";
+import { paginateReviews } from "./assets/helpers/paginateReviews.js";
 
 export default function AppContent() {
-  const { product, setProduct, fetchProduct } = useContext(ContextVariables);
+  const { product, setProduct, fetchProduct, setPaginatedData } = useContext(ContextVariables);
 
   useEffect(() => {
     async function loadProduct() {
       const slug = window.location.pathname.replace("/products/", ``);
       const data = await fetchProduct(slug);
       setProduct(data);
+      if(data.reviews) setPaginatedData(paginateReviews(data.reviews));
     }
     loadProduct();
     getVisitorId();
