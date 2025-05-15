f01a70af77cc1dbadbc64fafe68f0b5894da1932
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 11:54:57 +0300

    calculate avarage reviews rate

--- src/assets/context-variables/ContextVariables.jsx
+++ src/assets/context-variables/ContextVariables.jsx
@@ -10,10 +10,14 @@ export const ContextProvider = ({children}) => {
     async function fetchProduct(productSlug) {
         const endpoint = url + `/product/` + productSlug;
         const res = await fetch(endpoint);
-
         if (!res.ok) return null;
         if(res.status == 200) {
             const data = await res.json();
+            if(data.reviews && data.reviews.length > 0){
+                const avarageReviewsRate = (data.reviews.reduce((sum, v) => sum + (v.rating || 0), 0)) / data.reviews.length;
+                data.avarageReviewsRate = avarageReviewsRate;
+            }
+
             return data
         }
         return undefined
