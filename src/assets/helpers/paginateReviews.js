a1c641d6f9c7aef215bb41eeab93211c06f92252
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 10:10:53 +0300

    Pagination helper

---
+++ src/assets/helpers/paginateReviews.js
@@ -0,0 +1,14 @@
+export function paginateReviews(reviewsArray) {
+    if (reviewsArray.length < 11) return [reviewsArray]
+    const result = [];
+    const iterations = Math.floor(reviewsArray.length / 10);
+
+    const desiredPageLength = 10;
+
+    for (let i = 0; i < iterations; i++) {
+        let page = reviewsArray.splice(0, desiredPageLength);
+        result.push(page);
+    }
+
+    return result;
+}
