32f994f6aefdc0f7d66bab4b49b9b1bf4d221bea
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-16 12:48:28 +0300

    new button

--- src/assets/Components/Pagination.jsx
+++ src/assets/Components/Pagination.jsx
@@ -10,8 +10,30 @@ export default function Pagination({ currentPage, paginatedData, setCurrentPageI
     }, [currentPageIndex])
     console.log(currentPageIndex)
 
-    return (
-        <div className={Style.paginationContainer}>
+            return (
+                <div className={Style.paginationContainer}>
+                {page > 1  && (
+                    <>
+
+                        <button
+                            
+                            className={Style.arrowBtn}
+                            onClick={() => setCurrentPageIndex((now) => now - 1)}
+                        >
+                            <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.5 6.78467H0.7" stroke="#1F221F" strokeWidth="0.8" />
+                                <path
+                                    d="M7.8593 12.6219C7.8593 12.6219 5.5684 6.78473 0.8064 6.78473C5.57 6.78473 7.8593 0.94751 7.8593 0.94751"
+                                    stroke="#1F221F"
+                                />
+                            </svg>
+                        </button>
+                    
+                    </>
+                    
+                )}
+
+
             {paginatedData.map((page, i) => (
                 <button
                     key={page}
